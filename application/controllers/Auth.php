a67124b79829cd497115e71d1372e70118c1f539
Muhammad Fajri <mfajri429@gmail.com>
2020-04-28 20:04:18 +0700

    modfikasi logika login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -14,9 +14,10 @@ class Auth extends CI_Controller {
 	{
 		cekLogout();
 		
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$user     = $this->M_user->tampil_perUsername($username)->row_array();
+		$username   = $this->input->post('username');
+		$password   = $this->input->post('password');
+		$user       = $this->M_user->tampil_perUsername($username)->row_array();
+		$user_valid = $this->M_user->tampil_perUsername($username)->num_rows();
 
 		$this->form_validation->set_rules('username', 'Username', 'required');
         $this->form_validation->set_rules('password', 'Password', 'required');
@@ -24,37 +25,40 @@ class Auth extends CI_Controller {
         if( $this->form_validation->run() == FALSE ){
     		$data['title'] = 'SIK-MAYPI';
 			$this->load->view('auth/login', $data);
-        } else if( $username == $user['username'] ) {
-        	if( password_verify($password, $user['password']) ){
-        		$data = ['username' => $user['username']];
+        } else {
+        	if( $user_valid == 1 ){
+	        	if( password_verify($password, $user['password']) ){
+	        		$data = ['username' => $user['username']];
 
-				$this->session->set_userdata($data);
+					$this->session->set_userdata($data);
 
-	        	redirect(base_url());
-	        } else {
-	        	$this->session->set_flashdata('message', '
-	        		<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
-					  <strong>Username / Password Salah!
-					  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-					    <span aria-hidden="true">&times;</span>
-					  </button>
-					</div>');
+		        	redirect(base_url());
+		        } else {
+		        	$this->session->set_flashdata('message', '
+		        		<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+						  <strong>Username / Password Salah!
+						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						    <span aria-hidden="true">&times;</span>
+						  </button>
+						</div>');
 
+		        	$data['title'] = 'SIK-MAYPI';
+					$this->load->view('auth/login', $data);
+		        }
+        	} else {
+        		$this->session->set_flashdata('message', '
+		        		<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+						  <strong>Anda bukan admin!
+						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						    <span aria-hidden="true">&times;</span>
+						  </button>
+						</div>');
+		        	
 	        	$data['title'] = 'SIK-MAYPI';
 				$this->load->view('auth/login', $data);
-	        }
-        } else {
-        	$this->session->set_flashdata('message', '
-	        		<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
-					  <strong>Anda bukan admin!
-					  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-					    <span aria-hidden="true">&times;</span>
-					  </button>
-					</div>');
-	        	
-        	$data['title'] = 'SIK-MAYPI';
-			$this->load->view('auth/login', $data);
-        }
+        	}
+
+	    }
 	}
 
 	public function logout()
