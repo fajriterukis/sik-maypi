2f02e67f240ba3ab011eb909571ed7c8009b3dda
Muhammad Fajri <mfajri429@gmail.com>
2020-06-10 19:52:00 +0700

    backup database

--- sik-maypi.sql
+++ sik-maypi.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2020 at 06:25 AM
+-- Generation Time: Jun 10, 2020 at 02:50 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -60,7 +60,7 @@ INSERT INTO `dana_keluar` (`id`, `keperluan`, `jenis_dana`, `tanggal`, `uang_kel
 (54, 'Pena 2 Kotak, Lem Kertas besar, Tisu Ukuran Sedang', 'Denda', '2020-03-15', 57000, '2019/2020'),
 (55, 'Bu Rini Pinjam', 'Denda', '2020-03-17', 50000, '2019/2020'),
 (56, 'Bensin service printer', 'Denda', '2020-03-19', 10000, '2019/2020'),
-(57, 'Bu Rini Pinjam 2', 'Denda', '2020-03-28', 300000, '2019/2020'),
+(57, 'Bu Rini Pinjam 2', 'Denda', '2020-03-28', 200000, '2019/2020'),
 (59, 'Emis', 'Denda', '2020-04-08', 100000, '2019/2020'),
 (60, 'Konsumsi saat Covid-19', 'Denda', '2020-04-11', 10000, '2019/2020'),
 (61, 'Karpet', 'Denda', '2020-04-13', 165000, '2019/2020'),
@@ -77,11 +77,16 @@ INSERT INTO `dana_keluar` (`id`, `keperluan`, `jenis_dana`, `tanggal`, `uang_kel
 (73, 'Bu Rini Beli Plastik', 'Denda', '2020-04-22', 5000, '2019/2020'),
 (74, 'Lifeboy Cair + Stela', 'Denda', '2020-05-03', 58000, '2019/2020'),
 (75, 'Fotocopy SK Kelulusan', 'Denda', '2020-05-04', 36000, '2019/2020'),
-(76, 'Fotocopy Soal MID', 'Denda', '2020-05-09', 245000, '2019/2020'),
+(76, 'Fotocopy Soal MID + Amplop F4', 'Denda', '2020-05-09', 245000, '2019/2020'),
 (77, 'Marjan Merah Coco Pandan Sedus', 'Denda', '2020-05-13', 198000, '2019/2020'),
-(78, 'Fajri Pinjam ', 'Denda', '2020-05-14', 52000, '2019/2020'),
 (79, 'Minyak Tropical 1 Liter 10 Sachet', 'Denda', '2020-05-14', 160000, '2019/2020'),
-(80, 'Susu Frisian Flag 10 Kaleng+Tisu Paseo', 'Denda', '2020-05-15', 145000, '2019/2020');
+(80, 'Susu Frisian Flag 10 Kaleng+Tisu Paseo', 'Denda', '2020-05-15', 145000, '2019/2020'),
+(81, 'Air Mineral Vin 1 dus', 'Denda', '2020-06-08', 16000, '2019/2020'),
+(82, 'Konsumsi', 'Denda', '2020-06-08', 25000, '2019/2020'),
+(83, 'Fotocopy Soal UAS+Formulir PPDB = 1885 Lembar', 'Denda', '2020-06-10', 377000, '2019/2020'),
+(84, 'Double Folio(5 pack), Amplop Folio Coklat(1 Pack), Cutter Besar', 'Denda', '2020-06-10', 153000, '2019/2020'),
+(85, 'Bu Novi Pinjam', 'Denda', '2020-06-10', 50000, '2019/2020'),
+(86, 'Masih dibawak Bu Wiwit Untuk Fotocopy', 'Denda', '2020-06-10', 47000, '2019/2020');
 
 -- --------------------------------------------------------
 
@@ -222,12 +227,101 @@ INSERT INTO `pelunasan_un` (`id`, `nama`, `tanggal`, `nominal`, `tahun_ajaran`) 
 
 CREATE TABLE `pembayaran_siswa` (
   `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `kelas` varchar(30) NOT NULL,
-  `tanggal` date NOT NULL,
-  `nominal` int(11) NOT NULL,
-  `tahun_ajaran` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nama` varchar(24) DEFAULT NULL,
+  `kelas` varchar(4) DEFAULT NULL,
+  `tanggal` varchar(30) DEFAULT NULL,
+  `nominal` int(11) DEFAULT NULL,
+  `tahun_ajaran` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran_siswa`
+--
+
+INSERT INTO `pembayaran_siswa` (`id`, `nama`, `kelas`, `tanggal`, `nominal`, `tahun_ajaran`) VALUES
+(1, 'Abim Sanjaya', 'X', '', 0, '2019/2020'),
+(2, 'Aji Pangestu', 'X', '', 0, '2019/2020'),
+(3, 'Andika', 'X', '', 0, '2019/2020'),
+(4, 'Bima Putra Wira Darma', 'X', '2020-06-10', 100000, '2019/2020'),
+(5, 'Dani Reza Pratama', 'X', '2020-06-10', 100000, '2019/2020'),
+(6, 'Dian Ari Pratama', 'X', '2020-06-10', 100000, '2019/2020'),
+(7, 'Doni Damara', 'X', '', 0, '2019/2020'),
+(8, 'Eko', 'X', '', 0, '2019/2020'),
+(9, 'Farhan Diki', 'X', '2020-06-10', 100000, '2019/2020'),
+(10, 'Hafis Dermawan', 'X', '', 0, '2019/2020'),
+(11, 'Herwin Afandi', 'X', '2020-06-10', 100000, '2019/2020'),
+(12, 'Kholisatun N.', 'X', '2020-06-10', 100000, '2019/2020'),
+(13, 'M. Amrulloh', 'X', '', 0, '2019/2020'),
+(14, 'M. Fajri', 'X', '2020-06-10', 100000, '2019/2020'),
+(15, 'M. Malik Rohman', 'X', '2020-06-10', 100000, '2019/2020'),
+(16, 'M. Muzaqi', 'X', '2020-06-10', 100000, '2019/2020'),
+(17, 'M. Rofiqul Hidayat', 'X', '', 0, '2019/2020'),
+(18, 'M. Sahrial', 'X', '', 0, '2019/2020'),
+(19, 'Masning', 'X', '', 0, '2019/2020'),
+(20, 'Miftahul Huda', 'X', '2020-06-10', 100000, '2019/2020'),
+(21, 'Nata', 'X', '', 0, '2019/2020'),
+(22, 'Priyo Wahyudi', 'X', '2020-06-10', 100000, '2019/2020'),
+(23, 'Putra Wijaya', 'X', '', 0, '2019/2020'),
+(24, 'Rahmansyah', 'X', '', 0, '2019/2020'),
+(25, 'Rendi', 'X', '', 0, '2019/2020'),
+(26, 'Robin Sanjaya', 'X', '', 0, '2019/2020'),
+(27, 'Rusmawati', 'X', '', 0, '2019/2020'),
+(28, 'Setio Budiono', 'X', '', 0, '2019/2020'),
+(29, 'Wahyu Dinata', 'X', '2020-06-10', 50000, '2019/2020'),
+(30, 'Yuni Astuti', 'X', '', 0, '2019/2020'),
+(31, 'Juandri Ardan', 'X', '', 0, '2019/2020'),
+(32, 'Rizki Dinata', 'X', '', 0, '2019/2020'),
+(33, 'Adi Cahyono', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(34, 'Ahmad Sodik', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(35, 'Ali Mustopa', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(36, 'Dandi Saputra', 'XI.1', '1970-01-01', 0, '2019/2020'),
+(37, 'Dea Nanda', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(38, 'Dea Septia', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(39, 'Dinda Novita Sari', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(40, 'Doni Kurnadi', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(41, 'Eli Nurul Nikmah', 'XI.1', '', 0, '2019/2020'),
+(42, 'Habib Nurohman', 'XI.1', '', 0, '2019/2020'),
+(43, 'Handoyo', 'XI.1', '', 0, '2019/2020'),
+(44, 'Hardiansyah', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(45, 'Istaib Suro', 'XI.1', '', 0, '2019/2020'),
+(46, 'Joni', 'XI.1', '', 0, '2019/2020'),
+(47, 'Nafa Yuhenda', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(48, 'Putri Irmawati', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(49, 'Ragil Alen Valensa', 'XI.1', '', 0, '2019/2020'),
+(50, 'Rani', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(51, 'Revi Mariska', 'XI.1', '', 0, '2019/2020'),
+(52, 'Riko Pratama', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(53, 'Rio Jatri Pratama', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(54, 'Sukron Arifin', 'XI.1', '', 0, '2019/2020'),
+(55, 'Syahrial', 'XI.1', '', 0, '2019/2020'),
+(56, 'Tika Sari', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(57, 'Yeni Julia', 'XI.1', '1970-01-01', 0, '2019/2020'),
+(58, 'Revi Oktavia', 'XI.1', '2020-06-10', 100000, '2019/2020'),
+(59, 'Ahmad Syahri', 'XI.2', '', 0, '2019/2020'),
+(60, 'Andri Budianto', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(61, 'Anggi Abdul Sahid', 'XI.2', '', 0, '2019/2020'),
+(62, 'Dicki Vernando', 'XI.2', '', 0, '2019/2020'),
+(63, 'Fajar Purnomo', 'XI.2', '', 0, '2019/2020'),
+(64, 'Fitriani', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(65, 'Hendra Surandi', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(66, 'Ibnu Malik', 'XI.2', '', 0, '2019/2020'),
+(67, 'Irvan Windarto', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(68, 'Jeri Novendra', 'XI.2', '', 0, '2019/2020'),
+(69, 'Jimmy', 'XI.2', '2020-06-10', 50000, '2019/2020'),
+(70, 'Lilis Ainun Fatkul Janah', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(71, 'M. Ngafifur Rosidin', 'XI.2', '', 0, '2019/2020'),
+(72, 'Menik Andarwati', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(73, 'Muhammad Suhendra', 'XI.2', '', 0, '2019/2020'),
+(74, 'Purwanti', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(75, 'Rano', 'XI.2', '', 0, '2019/2020'),
+(76, 'Robi Malvino', 'XI.2', '', 0, '2019/2020'),
+(77, 'Siti Fatimah', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(78, 'Surya Adi Wijaya', 'XI.2', '', 0, '2019/2020'),
+(79, 'Tania Wulandari', 'XI.2', '', 0, '2019/2020'),
+(80, 'Wahid Rama Oktafian', 'XI.2', '', 0, '2019/2020'),
+(81, 'Wardatun Nuriya', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(82, 'Widia Ningsih', 'XI.2', '2020-06-10', 100000, '2019/2020'),
+(83, 'Yeni Ika Violita', 'XI.2', '2020-06-10', 100000, '2019/2020');
 
 -- --------------------------------------------------------
 
@@ -345,7 +439,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `dana_keluar`
 --
 ALTER TABLE `dana_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
 --
 -- AUTO_INCREMENT for table `dana_keluar_un`
@@ -369,7 +463,7 @@ ALTER TABLE `pelunasan_un`
 -- AUTO_INCREMENT for table `pembayaran_siswa`
 --
 ALTER TABLE `pembayaran_siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT for table `pengaturan`
