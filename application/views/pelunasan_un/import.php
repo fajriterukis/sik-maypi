6128d4e9d2b13c9408f0b4c81f873675be0b7359
Muhammad Fajri <mfajri429@gmail.com>
2022-06-30 03:31:42 +0700

    membuat halaman import pelunasan un

---
+++ application/views/pelunasan_un/import.php
@@ -0,0 +1,18 @@
+<div class="row">
+	<div class="col-md-6">
+		<div class="card shadow mb-4">
+			<div class="card-header py-3">
+			  <h6 class="m-0 font-weight-bold text-primary float-left">Import Pelunasan UN</h6>
+			</div>
+			<div class="card-body">
+				<form action="upload" method="POST" enctype="multipart/form-data">
+			    <a href="" class="tombol-download btn btn-success float-left">Download template file untuk diimport</a>
+				  <div class="form-group">
+				    <input type="file" class="form-upload form-control-file float-right mt-3 mb-3" name="importexcel" accept=".xlsx,.xls" required>
+			      </div>
+			  	<button type="submit" class="btn btn-primary float-right">Import</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
